6ef342cd8f373a2c61c1e367ecf58950b7868879
Pink-Hat-Hacker <zyv@udel.edu>
2023-10-24 21:13:15 -0400

    solved errors. basic logic set up correct

--- src/Components/solve.ts
+++ src/Components/solve.ts
@@ -1,89 +1,60 @@
-// export function solvePuzzle(letters: string[], getNYTSol: boolean) {
-//     let solution = [];
-//     if (getNYTSol) {
-//         solution = () => {
-//             axios.get('/puzzles/letter-boxed').then((response: any) => {
-//                 const htmlContent = response.data;
-//                 // Define a regular expression to match the JSON containing "sides"
-//                 const regex = /window\.gameData\s*=\s*({[^}]*})/;
-//                 // Find and extract the JSON object containing "sides"
-//                 const match = htmlContent.match(regex);
-//                 if (match && match[1]) {
-//                   const jsonData = JSON.parse(match[1]);
-//                   const sol = jsonData.ourSolution;
-//                   return sol;
-//                 } else {
-//                   console.log('No match found for gameData in the HTML content.');
-//                 }
-//             })
-//               .catch((error: any) => {
-//                 console.error('Error fetching letters:', error);
-//             });
-//         }
-//     } else {
-
-//     }
-// }
-
 import fs from 'fs'; // Import the file system module for reading wordlist.txt
 import axios from 'axios'; // Use axios for web scraping
 
-// Function to scrape the NYT website and access wordlist.txt for solutions
-const solvePuzzle = async (letters: string[], autofill: boolean): Promise<JSON> => {
-  let sides: string[] = [];
-  let nytSolution: string[] = [];
-  if (autofill) {
-    nytSolution = axios.get('/puzzles/letter-boxed').then((response: any) => {
-        const htmlContent = response.data;
-        // Define a regular expression to match the JSON containing "sides"
-        const regex = /window\.gameData\s*=\s*({[^}]*})/;
-        // Find and extract the JSON object containing "sides"
-        const match = htmlContent.match(regex);
-        if (match && match[1]) {
-            const jsonData = JSON.parse(match[1]);
-            const sol = jsonData.ourSolution;
-            return sol;
-        } else {
-            console.log('No match found for gameData in the HTML content.');
-        }
-    })
-    .catch((error: any) => {
-        console.error('Error fetching nyt solution:', error);
-    });
-  } else if (letters) {
-    // Convert the letters into groups of 3
-    sides = convertLettersToGroups(letters);
-  }
 
-  // Access wordlist.txt to find solutions
-  const wordlist = loadWordlist();
-  const listOfSolutions = findSolutions(wordlist, sides);
-  console.log('Solutions:', listOfSolutions);
 
-  return {"Letters": sides, "NYT Solution": nytSolution, "Solutions List": listOfSolutions};
-};
+export function solvePuzzle(letters: string[], autofill: boolean) {
+    if (autofill) {
+        // For now, we'll just initialize empty results for demonstration
+        const sides = splitIntoGroups(letters, 3);
+        const nytSolution = axios.get('/puzzles/letter-boxed').then((response: any) => {
+            const htmlContent = response.data;
+            // Define a regular expression to match the JSON containing "sides"
+            const regex = /window\.gameData\s*=\s*({[^}]*})/;
+            // Find and extract the JSON object containing "sides"
+            const match = htmlContent.match(regex);
+            if (match && match[1]) {
+                const jsonData = JSON.parse(match[1]);
+                const sol = jsonData.ourSolution;
+                return sol;
+            } else {
+                console.log('No match found for gameData in the HTML content.');
+            }
+        })
+        .catch((error: any) => {
+            console.error('Error fetching nyt solution:', error);
+        });
 
-// Function to convert letters to groups of 3
-const convertLettersToGroups = (letters: string[]): string[] => {
-  const groups = [];
-  for (let i = 0; i < letters.length; i += 3) {
-    groups.push(letters.slice(i, i + 3).join(''));
-  }
-  return groups;
-};
+        return {
+            'Letters': sides,
+            'NYT Solution': nytSolution,
+            'Solution List': solveWithWordlist(sides),
+        };
+    } else {
+        // Split the given letters into groups of 3
+        const letterGroups = splitIntoGroups(letters, 3);
+        // Access the wordlist.txt to solve two-word solutions
+        const solutionList = solveWithWordlist(letterGroups);
 
-// Function to load wordlist from wordlist.txt
-const loadWordlist = (): string[] => {
-  // Read the wordlist.txt file and split it into an array of words
-  const wordlist = fs.readFileSync('wordlist.txt', 'utf-8').split('\n');
-  return wordlist;
-};
+        return {
+            'Letters': letterGroups,
+            'NYT Solution': [],
+            'Solution List': solutionList,
+        };
+    }
+}
 
-// Function to find solutions in the wordlist
-const findSolutions = (wordlist: string[], sides: string[]): string[] => {
-  // Implement your logic to find solutions using the wordlist and sides
-  // Return an array of found solutions
-  return [];
-};
+function splitIntoGroups(letters: string[], groupSize: number) {
+    const letterGroups = [];
+    for (let i = 0; i < letters.length; i += groupSize) {
+        letterGroups.push(letters.slice(i, i + groupSize).join(''));
+    }
+    return letterGroups;
+}
 
-export { solvePuzzle };
+function solveWithWordlist(letterGroups: string[]) {
+    // Access wordlist.txt to solve two-word solutions using letterGroups
+    const wordlist = fs.readFileSync('wordlist.txt', 'utf-8').split('\n');
+    // For now, we'll return an empty list for demonstration
+    return [];
+}
