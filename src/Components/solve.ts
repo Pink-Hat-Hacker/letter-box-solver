c81af68739f1ec0e4dc08ba3660c61255b2359eb
Pink-Hat-Hacker <zyv@udel.edu>
2023-10-25 11:48:40 -0400

    exporting wordlist as a const instead of a txt bc fs not working wihtout server

--- src/Components/solve.ts
+++ src/Components/solve.ts
@@ -1,17 +1,12 @@
-import fs from 'fs'; // Import the file system module for reading wordlist.txt
 import axios from 'axios'; // Use axios for web scraping
-
-
+import { wordlist } from './wordlist';
 
 export function solvePuzzle(letters: string[], autofill: boolean) {
     if (autofill) {
-        // For now, we'll just initialize empty results for demonstration
         const sides = splitIntoGroups(letters, 3);
         const nytSolution = axios.get('/puzzles/letter-boxed').then((response: any) => {
             const htmlContent = response.data;
-            // Define a regular expression to match the JSON containing "sides"
             const regex = /window\.gameData\s*=\s*({[^}]*})/;
-            // Find and extract the JSON object containing "sides"
             const match = htmlContent.match(regex);
             if (match && match[1]) {
                 const jsonData = JSON.parse(match[1]);
@@ -31,11 +26,9 @@ export function solvePuzzle(letters: string[], autofill: boolean) {
             'Solution List': solveWithWordlist(sides),
         };
     } else {
-        // Split the given letters into groups of 3
+        // NOT autofill nyt solution
         const letterGroups = splitIntoGroups(letters, 3);
-        // Access the wordlist.txt to solve two-word solutions
         const solutionList = solveWithWordlist(letterGroups);
-
         return {
             'Letters': letterGroups,
             'NYT Solution': [],
@@ -53,8 +46,6 @@ function splitIntoGroups(letters: string[], groupSize: number) {
 }
 
 function solveWithWordlist(letterGroups: string[]) {
-    // Access wordlist.txt to solve two-word solutions using letterGroups
-    const wordlist = fs.readFileSync('wordlist.txt', 'utf-8').split('\n');
-    // For now, we'll return an empty list for demonstration
+    console.log(wordlist);
     return [];
 }
