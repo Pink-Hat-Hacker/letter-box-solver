596e8023b129e91195a307dfc150f64152758bf0
Pink-Hat-Hacker <zyv@udel.edu>
2023-11-06 18:18:36 -0500

    solver working, going to change a lot to make it my own

--- src/Components/TrieNode.ts
+++ src/Components/TrieNode.ts
@@ -1,27 +1,29 @@
-class TrieNode {
-    children: Map<string, TrieNode>;
+import { useState, useCallback, useRef } from "react";
+
+type TrieNode = {
+    children: { [key: string]: TrieNode };
     isEndOfWord: boolean;
 
-    constructor() {
-        this.children = new Map();
-        this.isEndOfWord = false;
-    }
-}
+};
 
 class Trie {
     root: TrieNode;
 
     constructor() {
-        this.root = new TrieNode();
+        this.root = this.createNode();
     }
 
-    insert(word: string) {
+    createNode(): TrieNode {
+        return { isEndOfWord: false, children: {} };
+    }
+
+    insert(word: string): void {
         let node = this.root;
         for (const char of word) {
-            if (!node.children.has(char)) {
-                node.children.set(char, new TrieNode());
+            if (!node.children[char]) {
+                node.children[char] = this.createNode();
             }
-            node = node.children.get(char)!;
+            node = node.children[char];
         }
         node.isEndOfWord = true;
     }
@@ -29,59 +31,219 @@ class Trie {
     search(word: string): boolean {
         let node = this.root;
         for (const char of word) {
-            if (!node.children.has(char)) {
+            if (!node.children[char]) {
                 return false;
             }
-            node = node.children.get(char)!;
+            node = node.children[char];
         }
         return node.isEndOfWord;
     }
 }
 
-export function findTwoWordSolutions(letterGroups: string[], wordlist: string[]): string[][] {
-    const solutions: string[][] = [];
-    const trie = new Trie();
+const findOneWords = (trie: Trie, letters: string[][]): string[][] => {
+    const results: string[][] = [];
+    const path: string[] = [];
 
+    const flatLetters = letters.flat();
+    const groups = flatLetters.map((letter) => {
+        return letters.findIndex((group) => group.includes(letter));
+    });
+
+    const dfs = (
+        node: TrieNode,
+        lastGroup: number,
+        used: Map<string, number>
+    ) => {
+        if (node.isEndOfWord) {
+            let count = 0;
+            for (const pair of used) {
+                if (pair[1] >= 1) {
+                    count += 1;
+                }
+            }
+            if (count === flatLetters.length) {
+                results.push([path.join("")]);
+            }
+        }
+
+        for (let i = 0; i < flatLetters.length; i++) {
+            const letter = flatLetters[i];
+            const group = groups[i];
+
+            if (node.children[letter] && lastGroup !== group) {
+                const currentCount = used.get(letter) || 0;
+                used.set(letter, currentCount + 1);
+                path.push(letter);
+                dfs(node.children[letter], group, used);
+                path.pop();
+                used.set(letter, currentCount);
+            }
+        }
+    };
+
+    const used: Map<string, number> = new Map<string, number>();
+    dfs(trie.root, -1, used);
+    return results;
+};
+
+const findTwoWords = (trie: Trie, letters: string[][]): string[][] => {
+    const results: string[][] = [];
+    const path1: string[] = [];
+    let path2: string[] = [];
+    const flatLetters = letters.flat();
+    const groups = flatLetters.map((letter) => {
+        return letters.findIndex((group) => group.includes(letter));
+    });
+
+    const dfsTwoWords = (
+        node: TrieNode,
+        lastGroup: number,
+        used2: Map<string, number>,
+        firstWord: string
+    ) => {
+        if (node.isEndOfWord) {
+            let count = 0;
+            for (const pair of used2) {
+                if (pair[1] >= 1) {
+                    count += 1;
+                }
+            }
+            if (count === flatLetters.length) {
+                results.push([firstWord, path2.join("")]);
+            }
+        }
+
+        for (let i = 0; i < flatLetters.length; i++) {
+            const letter = flatLetters[i];
+            const group = groups[i];
+
+            if (node.children[letter] && lastGroup !== group) {
+                const currentCount = used2.get(letter) || 0;
+                used2.set(letter, currentCount + 1);
+                path2.push(letter);
+                dfsTwoWords(node.children[letter], group, used2, firstWord);
+                path2.pop();
+                used2.set(letter, currentCount);
+            }
+        }
+    };
+
+    const dfsOneWord = (
+        node: TrieNode,
+        lastGroup: number,
+        used1: Map<string, number>
+    ) => {
+        if (node.isEndOfWord) {
+            const firstWord = path1.join("");
+            const firstLetter = firstWord[firstWord.length - 1];
+            if (trie.root.children[firstLetter]) {
+                path2 = [firstLetter];
+                const used2 = new Map<string, number>([...used1]);
+                const currentCount = used2.get(firstLetter) || 0;
+                used2.set(firstLetter, currentCount + 1);
+                dfsTwoWords(
+                    trie.root.children[firstLetter],
+                    -1,
+                    used2,
+                    firstWord
+                );
+            }
+        }
+
+        for (let i = 0; i < flatLetters.length; i++) {
+            const letter = flatLetters[i];
+            const group = groups[i];
+
+            if (node.children[letter] && lastGroup !== group) {
+                const currentCount = used1.get(letter) || 0;
+                used1.set(letter, currentCount + 1);
+                path1.push(letter);
+                dfsOneWord(node.children[letter], group, used1);
+                path1.pop();
+                used1.set(letter, currentCount);
+            }
+        }
+    };
+    const used = new Map<string, number>();
+    dfsOneWord(trie.root, -1, used);
+    return results;
+};
+
+
+export function Solver(letters: string[][], wordlist: string[]): [string[][], () => void] {
+    const [answers, setAnswers] = useState<string[][]>([]);
+    const trieRef = useRef<Trie | null>(null);
+
+    const newTrie = new Trie();
     for (const word of wordlist) {
-        trie.insert(word);
+        newTrie.insert(word);
     }
+    trieRef.current = newTrie;
 
-    function backtrack(path: string[], currentSide: string[], unusedSides: string[][]): void {
-        if (currentSide.length === 0) {
-            const word = path.join('');
-            console.log("Word: " + word);
-            if (trie.search(word)) {
-                solutions.push([path.join("")]);
-                console.log("(line 55) Solutions: " + solutions);
-            }
-            return;
+    const computeAnswers = useCallback(() => {
+        if (trieRef.current) {
+            const newAnswers = findOneWords(trieRef.current, letters).concat(
+                findTwoWords(trieRef.current, letters)
+            );
+            setAnswers(newAnswers);
         }
-
-        for (let i = 0; i < currentSide.length; i++) {
-            if (path.length > 0 && path[path.length - 1].endsWith(currentSide[i])) {
-                console.log("Path: " + path);
-                continue; // Avoid consecutive letters
-            }
-
-            const newSide = currentSide.slice();
-            const newPath = path.slice();
-            newPath.push(currentSide[i]);
-            newSide.splice(i, 1);
-
-            backtrack(newPath, newSide, unusedSides);
+    }, [letters]);
+    
+    const solve = () => {
+        if (trieRef.current) {
+            computeAnswers();
         }
+    };
 
-        if (unusedSides.length > 0) {
-            const newSide = unusedSides[0];
-            const newUnusedSides = unusedSides.slice(1);
-            backtrack([...path], newSide, newUnusedSides);
-        }
-    }
-
-    const [top, right, bottom, left] = letterGroups.map((group) => Array.from(group));
-    console.log("Sides: " + top, right, bottom, left);
-    backtrack([], top, [right, bottom, left]);
-
-    return solutions;
+    return [answers, solve];
 }
+
+
+
+// export function findTwoWordSolutions(letterGroups: string[], wordlist: string[]): string[][] {
+//     const solutions: string[][] = [];
+//     const trie = new Trie();
+
+//     for (const word of wordlist) {
+//         trie.insert(word);
+//     }
+
+//     function backtrack(path: string[], currentSide: string[], unusedSides: string[][]): void {
+//         if (currentSide.length === 0) {
+//             const word = path.join('');
+//             // console.log("Word: " + word);
+//             if (trie.search(word)) {
+//                 solutions.push([path.join("")]);
+//                 // console.log("(line 55) Solutions: " + solutions);
+//             }
+//             return;
+//         }
+
+//         for (let i = 0; i < currentSide.length; i++) {
+//             if (path.length > 0 && path[path.length - 1].endsWith(currentSide[i])) {
+//                 // console.log("Path: " + path);
+//                 continue; // Avoid consecutive letters
+//             }
+
+//             const newSide = currentSide.slice();
+//             const newPath = path.slice();
+//             newPath.push(currentSide[i]);
+//             newSide.splice(i, 1);
+
+//             backtrack(newPath, newSide, unusedSides);
+//         }
+
+//         if (unusedSides.length > 0) {
+//             const newSide = unusedSides[0];
+//             const newUnusedSides = unusedSides.slice(1);
+//             backtrack([...path], newSide, newUnusedSides);
+//         }
+//     }
+
+//     const [top, right, bottom, left] = letterGroups.map((group) => Array.from(group));
+//     // console.log("Sides: " + top, right, bottom, left);
+//     backtrack([], top, [right, bottom, left]);
+//     console.log(solutions);
+//     return solutions;
+// }
 
--- src/Components/solve.ts
+++ src/Components/solve.ts
@@ -1,20 +1,23 @@
 import axios from "axios"; // Use axios for web scraping
 import { wordlist } from "./wordlist";
-import { findTwoWordSolutions } from "./TrieNode";
+// import { Solver } from "./TrieNode";
+// import { useState, useEffect } from "react";
+
 
 export async function SolvePuzzle(letters: string[], autofill: boolean) {
   // if user autofilled, look for the nyt solution
+  const sides = splitIntoGroups(letters, 3);
   if (autofill) {
-    const sides = splitIntoGroups(letters, 3);
     const nytSolution = await fetchNYTSolution();
-    // return a json with the sides, nytSolution found and the list of solves
     const solutionList = solveWithWordlist(sides);
+    
     console.log(
       "Letters: %s \n NYT Solution: %s \n Solution List: %s",
       sides,
       nytSolution,
       solutionList
     );
+
     return {
       Letters: sides,
       "NYT Solution": nytSolution,
@@ -22,10 +25,10 @@ export async function SolvePuzzle(letters: string[], autofill: boolean) {
     };
   } else {
     // NOT autofill nyt solution
-    const letterGroups = splitIntoGroups(letters, 3);
-    const solutionList: string[][] = solveWithWordlist(letterGroups);
+    const solutionList: string[][] = solveWithWordlist(sides);
+    
     return {
-      Letters: letterGroups,
+      Letters: sides,
       "NYT Solution": [],
       "Solution List": solutionList,
     };
@@ -52,16 +55,116 @@ async function fetchNYTSolution() {
   }
 }
 
-function splitIntoGroups(letters: string[], groupSize: number): string[] {
-  const letterGroups = [];
-  for (let i = 0; i < letters.length; i += groupSize) {
-    letterGroups.push(letters.slice(i, i + groupSize).join(""));
-  }
-  return letterGroups;
+function splitIntoGroups(letters: string[], groupSize: number): string[][] {
+    const result: string[][] = [];
+    for (let i = 0; i < letters.length; i += groupSize) {
+        result.push(letters.slice(i, i + groupSize));
+    }
+    return result;
 }
 
-function solveWithWordlist(letterGroups: string[]): string[][] {
-//   const wl = new Set(wordlist);
-  const result = findTwoWordSolutions(letterGroups, wordlist);
-  return result;
+function isValidWord(word: string, letterGroups: string[][]): boolean {
+    const A: Set<string> = new Set(letterGroups[0]);
+    const B: Set<string> = new Set(letterGroups[1]);
+    const C: Set<string> = new Set(letterGroups[2]);
+    const D: Set<string> = new Set(letterGroups[3]);
+    const allLetters = new Set([...A, ...B, ...C, ...D]);
+
+    if (!word) return false;
+
+    let prevSet: Set<string> | null = null;
+    for (const letter of word) {
+        if (!allLetters.has(letter)) {
+            return false;
+        }
+
+        let currentSet: Set<string> | null = null;
+        if (A.has(letter)) currentSet = A;
+        else if (B.has(letter)) currentSet = B;
+        else if (C.has(letter)) currentSet = C;
+        else if (D.has(letter)) currentSet = D;
+
+        if (prevSet === currentSet) {
+            return false;
+        }
+
+        prevSet = currentSet;
+    }
+
+    return true;
+}
+
+function countDistinctLetters(word: string): number {
+    const usedLetters = new Set<string>();
+    for (const letter of word) {
+        usedLetters.add(letter);
+    }
+    return usedLetters.size;
+}
+
+function countDistinctLettersInPairs(...words: string[]): number {
+    const combinedWord = words.join('');
+    return countDistinctLetters(combinedWord);
+}
+
+function solveWithWordlist(letterGroups: string[][]) {
+    const allLetters = new Set([...letterGroups[0], ...letterGroups[1], ...letterGroups[2], ...letterGroups[3]]);
+    const acceptableWords: string[] = wordlist.filter((w: string): boolean => isValidWord(w, letterGroups));
+
+    let longestWord = '';
+    let mostDistinctWord = '';
+    let maxDistinctLetters = 0;
+    let bestPair: [string, string] = ['', ''];
+    let maxDistinctLettersInPair = 0;
+    let bestTriple: [string, string, string] = ['', '', ''];
+    let maxDistinctLettersInTriple = 0;
+
+    for (const word of acceptableWords) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    
+        const distinctLetters = countDistinctLetters(word);
+        if (distinctLetters > maxDistinctLetters) {
+            maxDistinctLetters = distinctLetters;
+            mostDistinctWord = word;
+        }
+    
+        // Find pairs of words
+        const lastLetter = word[word.length - 1];
+        const matchingWords = acceptableWords.filter((w) => w[0] === lastLetter);
+        for (const match of matchingWords) {
+            const distinctLettersInPair = countDistinctLettersInPairs(word, match);
+            if (distinctLettersInPair > maxDistinctLettersInPair) {
+                maxDistinctLettersInPair = distinctLettersInPair;
+                bestPair = [word, match];
+            }
+        }
+    }
+    
+    if (maxDistinctLettersInPair < allLetters.size) {
+        for (const [word1, word2] of [bestPair]) {
+            const lastLetter = word2[word2.length - 1];
+            const matchingWords = acceptableWords.filter((w) => w[0] === lastLetter);
+            for (const match of matchingWords) {
+                const distinctLettersInTriple = countDistinctLettersInPairs(word1, word2, match);
+                if (distinctLettersInTriple > maxDistinctLettersInTriple) {
+                    maxDistinctLettersInTriple = distinctLettersInTriple;
+                    bestTriple = [word1, word2, match];
+                }
+            }
+        }
+    }
+    
+    console.log(`Number of acceptable words: ${acceptableWords.length}`);
+    console.log(`Longest acceptable word: ${longestWord}`);
+    console.log(`Word with most distinct letters: ${mostDistinctWord}`);
+    
+    if (maxDistinctLettersInPair === allLetters.size) {
+        console.log(`Word pair covers all distinct letters: (${bestPair[0]}, ${bestPair[1]})`);
+    } else {
+        console.log(`Word triple with most distinct letters: (${bestTriple[0]}, ${bestTriple[1]}, ${bestTriple[2]})`);
+    }
+
+    return[[],[]];
 }
